05f41cd80deea3ac9a8d4ba6b9575d36bb8825ba
Abhishek Dam <abhishekdam852@gmail.com>
2021-05-12 01:31:12 +0530

    here i have added the css and few components

--- learning-react/src/index.js
+++ learning-react/src/index.js
@@ -1,17 +1,53 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDom from "react-dom";
+import "./index.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+function BookList() {
+  return (
+    <section className="booklist">
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  );
+}
+
+const Book = () => {
+  return (
+    <article className="book">
+      <Image></Image>
+      <Title></Title>
+      <Author></Author>
+    </article>
+  );
+};
+const Image = () => (
+  <img
+    src="https://images-eu.ssl-images-amazon.com/images/I/81YOuOGFCJL._AC_UL200_SR200,200_.jpg"
+    alt=""
+  />
+);
+const Title = () => <h2>Harry Potter and the Philosopher's Stone</h2>;
+const Author = () => (
+  <h3 style={{ color: "green", fontSize: "0.75rem", margin: "0.25rem" }}>
+    {" "}
+    {/** here we are applying the line CSS (But you like external CSS */} J.K.
+    Rowling
+  </h3>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// New task
+
+// function Welcome(props) {
+//   return <h1>Welcome, {props.name}</h1>;
+// }
+
+// end task
+
+ReactDom.render(<BookList />, document.getElementById("root"));
